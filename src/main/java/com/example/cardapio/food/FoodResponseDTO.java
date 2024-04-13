b374f16e7d0024c54df267e2f6457cd3c1398651
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-13 12:20:07 -0300

    Feat:criando rotas, fazendo conexão do banco , criação de entidades do banco

---
+++ src/main/java/com/example/cardapio/food/FoodResponseDTO.java
@@ -0,0 +1,8 @@
+package com.example.cardapio.food;
+
+public record FoodResponseDTO(Long id, String image, String title, Integer price) {
+
+    public  FoodResponseDTO(Food food){
+        this(food.getId(), food.getImage(), food.getTitle(), food.getPrice());
+    }
+}
